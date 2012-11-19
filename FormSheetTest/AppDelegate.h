1d39211a170bb9b9f6a460688f41da03510a3ca5
Daniel Skates <daniel@xcitelogic.com.au>
2012-11-19 16:08:19 +0800

    Initial Commit

---
+++ FormSheetTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FormSheetTest
+//
+//  Created by Daniel Skates on 19/11/12.
+//  Copyright (c) 2012 Daniel Skates. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FormSheetTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FormSheetTest
+//
+//  Created by Daniel Skates on 19/11/12.
+//  Copyright (c) 2012 Daniel Skates. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
